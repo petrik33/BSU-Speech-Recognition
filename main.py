749a80c8e9b353e9bc2a4a36a6074d7d3252f6d4
Timofey Petrikevich <petrik3003@gmail.com>
2023-11-19 21:17:34 +0300

    feat: main model

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import pyaudio
+from vosk import Model, KaldiRecognizer
+
+model = Model('models/ru')
+rec = KaldiRecognizer(model, 16000)
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+def listen():
+    while True:
+        try:
+            data = stream.read(4000, exception_on_overflow=False)
+            
+            if rec.AcceptWaveform(data) and len(data) > 0:
+                answer = json.loads(rec.Result())
+                if answer['text']:
+                    yield answer['text']
+        except KeyboardInterrupt:
+            continue
+
+try:
+    for text in listen():
+        print(text)
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
