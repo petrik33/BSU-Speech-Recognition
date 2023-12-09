f651b6fd967851824adc259a59b45ba657755538
Timofey Petrikevich <petrik3003@gmail.com>
2023-12-09 12:42:10 +0300

    feat: iteration 2

---
+++ iteration2.py
@@ -0,0 +1,20 @@
+from vosk import Model, KaldiRecognizer
+import wave
+
+model = Model("models/ru")  # Загрузка модели для русского языка
+rec = KaldiRecognizer(model, 16000)
+
+# Открытие аудиофайла
+wf = wave.open("recorded_audio.wav", "rb")
+
+# Чтение всех данных из аудиофайла
+data = wf.readframes(wf.getnframes())
+
+# Обработка данных через Vosk
+rec.AcceptWaveform(data)
+
+# Получение результата
+result = rec.FinalResult()
+
+# Вывод распознанного текста
+print(result)
